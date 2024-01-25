a00709cda5aa159ef92e394c0bf4d735e35d9e2a
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2024-01-25 16:38:34 +0100

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,8 @@
 
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
     const totalWidth = contentWidth + paddingWidth + borderWidth;
     return totalWidth;
 }
