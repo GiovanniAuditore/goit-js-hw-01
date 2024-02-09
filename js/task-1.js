985f8ad7ca391c667f5c915371393d2313c624db
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2024-02-09 19:33:16 +0100

    ард

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,11 @@
-
 'use strict';
 
 function slugify(title) {
     const slug = title.toLowerCase();
-    const arr = Array.from(slug.split("-"));
-    return arr
+    const arr = Array.from(slug.split(" "));
+    const result = arr.join("-"); 
+    return result;
 }
-
 
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
