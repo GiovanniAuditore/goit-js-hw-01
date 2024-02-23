5b2fe24fefff5a6f105e8c50961e020fc035e65b
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2024-02-23 10:31:19 +0100

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,34 @@
+
 'use strict';
 
-function slugify(title) {
-    const slug = title.toLowerCase();
-    const arr = Array.from(slug.split(" "));
-    const result = arr.join("-"); 
-    return result;
+
+function isEnoughCapacity(products, containerSize) {
+    let totalQuantity = 0;
+
+    // Перебираємо об'єкт products і додаємо кількість кожного товару до totalQuantity
+    for (const quantity of Object.values(products)) {
+        totalQuantity += quantity;
+    }
+
+    // Перевіряємо, чи загальна кількість товарів менше або дорівнює containerSize
+    return totalQuantity <= containerSize;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
+
+
