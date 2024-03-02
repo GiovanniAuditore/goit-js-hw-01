09f4b24e9fd4102c63339ee80d6ea4a8d67ce84b
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2024-03-02 17:14:24 +0100

    upd

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,2 @@
 "use strict";
 
-const car = {
-  brand: "Porsche",
-  speed: 0,
-  acceleration(){
-    this.speed +=10;
-    console.log(this.speed);
-
-  }
-  stoping(){
-    if (this.speed >= 0){
-      this.speed -= 10;
-    }
-    else(
-      console.log("No speed")
-    )
-  }
-}
