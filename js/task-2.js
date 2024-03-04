d53870f6fcda48bdecba771480e137a81660f3e8
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2024-03-04 16:14:35 +0100

    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,34 @@
 'use strict';
+
+
+class Storage {
+    #items
+    constructor(items) {
+      this.#items = items;
+    }
+    addItem(items) {
+        this.#items.push(items)
+
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    removeItem(item){
+        this.#items = this.#items.filter(function(existingItem) {
+            return existingItem !== item;})
+    }
+    }
+
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
